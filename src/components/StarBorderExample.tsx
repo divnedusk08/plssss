0d0410db823702418385f7c963b6f41df017320a
Dhriti Erusalagandi <dhriti@MacBookAir.attlocal.net>
2025-07-08 13:38:50 -0500

    fix: update StarBorderExample.tsx (was utils, not a component)

--- src/components/StarBorderExample.tsx
+++ src/components/StarBorderExample.tsx
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import path from "path";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
